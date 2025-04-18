0b0a09bc5594b42c1e6b5986b181990cc8dcbd4b
alex <apribylov90@mail.ru>
2025-04-18 17:50:43 +0300

    added login tests

---
+++ src/test/java/ru/alex/tests/LoginTests.java
@@ -0,0 +1,61 @@
+package ru.alex.tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.alex.models.auth.LoginErrorResponseModel;
+import ru.alex.models.auth.LoginRequestModel;
+import ru.alex.models.auth.LoginSuccessfulResponseModel;
+
+import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static ru.alex.specs.Specs.requestSpec;
+import static ru.alex.specs.Specs.responseSpec;
+
+@DisplayName("Проверка Login API")
+public class LoginTests extends BaseTest{
+
+    @DisplayName("Удачный вход пользователем")
+    @Test
+    public void loginSuccessfulTest() {
+        LoginRequestModel loginRequestBody = new LoginRequestModel();
+        loginRequestBody.setEmail("eve.holt@reqres.in");
+        loginRequestBody.setPassword("cityslicka");
+
+        LoginSuccessfulResponseModel response = given(requestSpec)
+                .body(loginRequestBody)
+                .when()
+                .post("/login")
+                .then()
+                .spec(responseSpec(200))
+                .extract().as(LoginSuccessfulResponseModel.class);
+
+
+        step("Проверка ответа", () -> {
+           assertThat(response.getToken()).isNotEmpty();
+        });
+    }
+
+    @DisplayName("Неудачный вход пользователем")
+    @Test
+    public void loginUnSuccessfulTest() {
+        LoginRequestModel loginRequestBody = new LoginRequestModel();
+        loginRequestBody.setEmail("test@test.com");
+        loginRequestBody.setPassword("test");
+
+        LoginErrorResponseModel response = given(requestSpec)
+                .body(loginRequestBody)
+                .when()
+                .post("/login")
+                .then()
+                .spec(responseSpec(400))
+                .extract().as(LoginErrorResponseModel.class);
+
+
+        step("Проверка ответа", () -> {
+            assertThat(response.getError()).isNotEmpty();
+            assertThat(response.getError()).isEqualTo("user not found");
+        });
+    }
+
+}
