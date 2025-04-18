8541ca7d9e17589a1e81fbb30480d4304e8978aa
alex <apribylov90@mail.ru>
2025-04-18 16:37:03 +0300

    refactoring; added specs

---
+++ src/test/java/ru/alex/specs/Specs.java
@@ -0,0 +1,23 @@
+package ru.alex.specs;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import static io.restassured.RestAssured.with;
+import static io.restassured.filter.log.LogDetail.ALL;
+
+public class Specs {
+    public static RequestSpecification requestSpec = with()
+            .filter(new AllureRestAssured())
+            .log().uri()
+            .contentType("application/json");
+
+    public static ResponseSpecification responseSpec(int statusCode) {
+        return new ResponseSpecBuilder()
+                .expectStatusCode(statusCode)
+                .log(ALL)
+                .build();
+    }
+}
--- src/test/java/ru/alex/tests/UsersTests.java
+++ src/test/java/ru/alex/tests/UsersTests.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.alex.models.user.UserResponseModel;
 
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.alex.specs.Specs.requestSpec;
+import static ru.alex.specs.Specs.responseSpec;
 
 @DisplayName("Проверка User API")
 public class UsersTests extends BaseTest {
@@ -18,21 +21,21 @@ public class UsersTests extends BaseTest {
         String expectedFirstName = "Janet";
         String expectedLastName = "Weaver";
 
-        UserResponseModel response = given()
-                .log().uri()
+        UserResponseModel response = given(requestSpec)
                 .when()
                 .get("/users/2")
                 .then()
-                .statusCode(200)
+                .spec(responseSpec(200))
                 .extract().as(UserResponseModel.class);
 
+        step("Проверка ответа", () -> {
+            assertThat(response.getData()).isNotNull();
+            assertThat(response.getSupport()).isNotNull();
 
-        assertThat(response.getData()).isNotNull();
-        assertThat(response.getSupport()).isNotNull();
-        assertThat(response.getSupport()).isNotNull();
+            assertEquals(expectedEmail, response.getData().getEmail());
+            assertEquals(expectedFirstName, response.getData().getFirstName());
+            assertEquals(expectedLastName, response.getData().getLastName());
+        });
 
-        assertEquals(expectedEmail, response.getData().getEmail());
-        assertEquals(expectedFirstName, response.getData().getFirstName());
-        assertEquals(expectedLastName, response.getData().getLastName());
     }
 }
